9d5a9782293e1059bf85ca8dd68c2b4e76122dfa
Juan Fox <foxj0875@gmail.com>
2018-05-28 13:37:02 -0400

    Flattens ingredientsToComoponents array so that length will be zero if all of props.ingredients keys have values of zero. Burger.js renders top and bottom bun with text prompting user to add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,17 +3,24 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
  const burger = (props) => {
-  const ingredientsToArray = Object.keys(props.ingredients)
+  let ingredientsToComponents = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((arraySlot, index) => {
         return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />;
       });
-    });
-    console.log(ingredientsToArray);
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if(ingredientsToComponents.length === 0) {
+    ingredientsToComponents= <p>Please add some ingredients</p>;
+  }
+  
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      {ingredientsToArray}
+      {ingredientsToComponents}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   )
