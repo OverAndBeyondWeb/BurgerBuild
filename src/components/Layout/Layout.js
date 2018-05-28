c4aa9993fa48a19db7f7a9f9206310579c652155
Juan Fox <foxj0875@gmail.com>
2018-05-27 20:37:13 -0400

    Layout.css is used to style Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Wrapper from '../../hoc/Wrapper';
+import classes from './Layout.css';
 
 const layout = (props) => {
   return (
     <Wrapper>
       <div>Blah, blah, blah</div>
-      <main>
+      <main className={classes.content}>
         {props.children}
       </main>
     </Wrapper>
