dd9ebdef0b3e0e883da431d11aa2e9743e5fcb8d
Juan Fox <foxj0875@gmail.com>
2018-05-29 14:23:34 -0400

    SideDrawer.js component re-uses NavigationItems component to render a slide out menu for mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Wrapper from '../../hoc/Wrapper';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
 const layout = (props) => {
   return (
     <Wrapper>
       <Toolbar/>
+      <SideDrawer/>
       <main className={classes.content}>
         {props.children}
       </main>
