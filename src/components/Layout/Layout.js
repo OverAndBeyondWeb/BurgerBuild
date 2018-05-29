a3e9d077d1d4048cc01c51d6781cbeca67c73d0c
Juan Fox <foxj0875@gmail.com>
2018-05-29 12:13:22 -0400

    Toolbar.js component is being rendered from Layout.js component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Wrapper from '../../hoc/Wrapper';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 
 const layout = (props) => {
   return (
     <Wrapper>
-      <div>Blah, blah, blah</div>
+      <Toolbar/>
       <main className={classes.content}>
         {props.children}
       </main>
